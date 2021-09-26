2fe4292dee23adb82e692f8bc9a4d505bafc113e
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2021-09-26 19:10:33 -0400

    changed ":" to "="

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-let pokemonList: [
+let pokemonList = [
     {name: 'Bulbasaur', height: 0.7,  type:['grass', 'poison']}, 
     {name: 'Charmander', height: 0.6, type: 'fire' },
     {name: 'Squirtle', height: 0.5, type: 'water'}
 ];
+

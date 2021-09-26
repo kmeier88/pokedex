e94f27887542112f49ea9f2f932f7223025f4575
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2021-09-26 18:58:24 -0400

    changed pokemonlist to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonlist: [
+let pokemonList: [
     {name: 'Bulbasaur', height: 0.7,  type:['grass', 'poison']}, 
     {name: 'Charmander', height: 0.6, type: 'fire' },
     {name: 'Squirtle', height: 0.5, type: 'water'}

aa91993c94f02c4e644c0b880eaec26b54a3dd4c
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2021-09-30 14:39:41 -0400

    for loop with conditionals for height

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@ let pokemonList = [
     {name: 'Squirtle', height: 0.5, type: 'water'}
 ];
 
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height <0.8 && pokemonList[i].height >0.4){
+        document.write(pokemonList[i].name + pokemonList[i].height + ' is an average sized pokemon'); 
+    }else if (pokemonList[i].height <0.4){
+                document.write(pokemonList[i].name + pokemonList[i].height + ' is a small pokemon');
+    }else 
+            document.write(pokemonList[i].name + pokemonList[i].height + ' is a large pokemon');
+    }
+    
+        

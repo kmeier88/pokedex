4377614df83e002d0b7fad44e56d9a8de2e13d5a
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2021-11-04 18:52:48 -0400

    modal

--- js/scripts.js
+++ js/scripts.js
@@ -81,5 +81,45 @@ pokemonRepository.getAll().forEach(function (pokemon) {
     });
 });
 
+let pokemonRepository = (function() {
+    let modalContainer = document.querySelector('#modal-container');
+    function showModal(title, text) {
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
 
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible'))
+    {
+        hideModal();
+    }
+});
+
+    document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal('Modal title', 'This is the modal content!');
+    });
     
+}) ();
